db3769a0aab50e94f5187a6702618268f9b33f74
hiungai <164631997+hiungai@users.noreply.github.com>
2024-10-17 07:46:37 +0000

    upload du lieu

---
+++ b2107120_php/major_add.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thêm chuyên ngành</title>
+</head>
+<body>
+    <h1>Thêm chuyên ngành</h1>
+    <form action="major_save.php" method="post">
+        Tên chuyên ngành: <input type="text" name="name_major" required>
+        <input type="submit" value="Thêm">
+    </form>
+</body>
+</html>
